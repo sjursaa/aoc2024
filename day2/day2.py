21b26a932279027b48ddce1856709131e7bc3034
Sjur <sjursaa@gmail.com>
2024-12-02 10:46:46 +0100

    implement tests for descending lists

--- day2/day2.py
+++ day2/day2.py
@@ -31,6 +31,20 @@ for line in file:
     # descending
     if list[0] > list[len(list)-1]:
         print("descending")
+        for i in range(1, len(list)):
+            #if turning point 
+            if (list[i-1] <= list[i]):
+                unsafe += 1
+                break
+            #if jump bigger than 4
+            if (int(list[i-1]) > (int(list[i]) + 3)):
+                unsafe += 1
+                break
+            else:
+                print("ok")
+        total += 1
 
 print(total)
 print(unsafe)
+print("-----")
+print(total - unsafe)
