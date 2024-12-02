1e3898c858faf074584359eca8f155f1fd240cb3
Sjur <sjursaa@gmail.com>
2024-12-02 10:36:43 +0100

    implement splitting of ascending vs descending list

--- day2/day2.py
+++ day2/day2.py
@@ -6,7 +6,16 @@ list = []
 for line in file:
     whole_line = line.strip()
     list = whole_line.split(" ")
+    print("new list")
     print(list)
+    print(list[0]) 
+    print(list[len(list)-1])
+    # ascending
+    if list[0] < list[len(list)-1]:
+        print("ascending")
 
-for line in list:
-    print(line)
+    # descending
+    if list[0] > list[len(list)-1]:
+        print("descending")
+
+
