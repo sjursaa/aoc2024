107f6782ddc3aa379029d8478e674d3745f19de6
Sjur <sjursaa@gmail.com>
2024-12-10 15:30:36 +0100

    implement solution for pt. 2 day 4. implement checks for character A, and check all 4 options

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,6 @@
 # file = open("exampledata.txt")
 # file = open("exampledata2.txt")
+# file = open("exampledata3.txt")
 file = open("data.txt")
 
 # init lists
@@ -83,3 +84,38 @@ for i, line in enumerate(array2d):
                 print(e, i, j)
 
 print(xmas)
+
+xmas2 = 0
+for i, line in enumerate(array2d):
+    for j, char in enumerate(line):
+        if char == 'A':
+            try:
+                # both M's on top
+                if array2d[i-1][j-1] == 'M' and array2d[i+1][j+1] == 'S' and array2d[i-1][j+1] == 'M' and array2d[i+1][j-1] == 'S' and j-1 >= 0 and i-1 >= 0:
+                    print("xmas for")
+                    xmas2 += 1
+            except IndexError as e:
+                print(e, i, j)
+            try:
+                # both M's on left
+                if array2d[i-1][j-1] == 'M' and array2d[i+1][j+1] == 'S' and array2d[i-1][j+1] == 'S' and array2d[i+1][j-1] == 'M' and j-1 >= 0 and i-1 >= 0:
+                    print("xmas for")
+                    xmas2 += 1
+            except IndexError as e:
+                print(e, i, j)
+            try:
+                # both M's on bottom
+                if array2d[i-1][j-1] == 'S' and array2d[i+1][j+1] == 'M' and array2d[i-1][j+1] == 'S' and array2d[i+1][j-1] == 'M' and j-1 >= 0 and i-1 >= 0:
+                    print("xmas for")
+                    xmas2 += 1
+            except IndexError as e:
+                print(e, i, j)
+            try:
+                # both M's on right
+                if array2d[i-1][j-1] == 'S' and array2d[i+1][j+1] == 'M' and array2d[i-1][j+1] == 'M' and array2d[i+1][j-1] == 'S' and j-1 >= 0 and i-1 >= 0:
+                    print("xmas for")
+                    xmas2 += 1
+            except IndexError as e:
+                print(e, i, j)
+
+print(xmas2)
