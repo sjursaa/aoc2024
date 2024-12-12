eeaf47e6284cf88081686fec46f702d6b844047c
Sjur <sjursaa@gmail.com>
2024-12-12 20:08:43 +0100

    day12: implement basic area and perimeter calculations for horizontal regions

--- day12/day12.py
+++ day12/day12.py
@@ -15,3 +15,38 @@ for line in file:
 
 for line in array2d:
     print(line)
+
+above_char = ""
+prev_char = ""
+list_of_things = []
+
+area = 1
+perimeter = 4
+for i, line in enumerate(array2d):
+    for j, char in enumerate(line):
+        if char == prev_char:
+            area += 1
+            perimeter += 2
+            tuple = (char, area, perimeter)
+            list_of_things.append(tuple)
+            print(tuple)
+        if char != prev_char:
+            # reinit values
+            area = 1
+            perimeter = 4
+            tuple = (char, area, perimeter)
+            list_of_things.append(tuple)
+
+        prev_char = char
+
+
+for i, line in enumerate(list_of_things):
+    print(i, line)
+        
+
+'''
+area * perimeter
+
+area = number of times character is consecutive
+
+'''
