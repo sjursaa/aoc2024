f56a44dc04539cdec9b4106695f984510903e650
Sjur <sjursaa@gmail.com>
2024-12-13 12:32:55 +0100

    day 13: implement successful algoritm for computing fewest number of tokens used (single case)

--- day13/day13.py
+++ day13/day13.py
@@ -1,18 +1,19 @@
 file = open("exampledata.txt")
 
 # TODO: init w/ 0, update w/ values from exampledata/data 
-button_a_x = 94
-button_a_y = 34
+button_a_x = 17
+button_a_y = 86
 
-button_b_x = 22
-button_b_y = 67
+button_b_x = 84
+button_b_y = 37
 
-prize_x = 8400
-prize_y = 5400
+prize_x = 7870
+prize_y = 6450
 
 cost_button_a = 3
 cost_button_b = 1
 
+"""
 # TODO: sketchy solution find modulus of smallest value divided normally
 print("result: ")
 print((button_a_x * 80) + (button_b_x * 40))
@@ -29,16 +30,34 @@ print(prize_x % button_a_x)
 print(prize_y % button_a_y)
 print(prize_x % button_b_x)
 print(prize_y % button_b_y) # modulus of lowest value, number of times button B must be pressed
-
+"""
 # compute remainder for other button to be pressed
-remainder_x = prize_x - (button_b_x * 40) 
-remainder_y = prize_y - (button_b_y * 40)
+# TODO: loop for each number 1-100
+remainder_x = 0
+remainder_y = 0
 
-print(remainder_x)
-print(remainder_y)
+for i in range(1,100):
+    # print("hello")
+    remainder_x = prize_x - (button_b_x * i) 
+    remainder_y = prize_y - (button_b_y * i)
 
-print(7520 / button_a_x) # 80
-print(2720 / button_a_y) # 80 
+    print(remainder_x)
+    print(remainder_y)
+
+    button_ax_presses = (remainder_x / button_a_x) # 80
+    button_ay_presses = (remainder_y / button_a_y) # 80 
+
+    print(button_ax_presses)
+    print(button_ay_presses)
+
+    if button_ax_presses == button_ay_presses:
+        print("bingbing")
+        print("button_b:", i)
+        print("button_a:", int(button_ax_presses))
+        tokens = (i + button_ax_presses*3)
+        print(tokens)
+        # break
+    print("nu")
 
 
 """
