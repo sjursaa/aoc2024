964eebf55f04be5734c67ad45b62b090bdc42de3
Sjur <sjursaa@gmail.com>
2024-12-19 20:01:10 +0100

    day 16: implement function for assigning towel patterns, sort by length of string (descending), start implementing checking for pattern in puzzle input

--- day19/day19.py
+++ day19/day19.py
@@ -1,23 +1,44 @@
 file = open("exampledata.txt")
+# file = open("data.txt")
 
 list = []
 def open_file():
     for line in file:
         list.append(line.strip())
-        # print(line.strip())
 
 def print_list():
     for i, item in enumerate(list):
         print(i, item)
     
 def assign_towel_patterns():
-    patterns = list[0].split(",")
-    return patterns
+    patterns = list[0].split(", ")
+    sorted_by_length = sorted(patterns, key=len)
+    sorted_by_length.reverse()
+    return sorted_by_length
+
+def check_if_possible(patterns):
+    print(patterns)
+    for i, item in enumerate(list):
+        if (i >= 2):
+            # for char in item:
+                # print(char)
+            print(i, item)
+            # TODO: check longest strings first
+            for pattern in patterns:
+                if item.startswith(pattern):
+                    print(item, pattern)
+                # print(pattern)
+            
+
+            
+        # print(item)
 
 if __name__ == "__main__":
     open_file()
     # print_list()
     patterns = assign_towel_patterns()
-    print(patterns)
-    print_list()
+    # print(patterns)
+    # print_list()
+    check_if_possible(patterns)
+
 
