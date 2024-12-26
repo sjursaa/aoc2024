e3cbf89850690a7bde7eb18212ea71ed89e7bb61
Sjur <sjursaa@gmail.com>
2024-12-26 20:32:37 +0100

    day 5: implement finding middle value per line in the ok list, sums together

--- day5/day5.py
+++ day5/day5.py
@@ -19,15 +19,38 @@ def check_order(rules, updates):
         last = rule.split("|")[1]
         print(first, last)
         for update in updates:
-            if first in update and last in update:
-                print("they both be in here mon", update)
-                # TODO: check if first appears before last in update
-                
+            update_list = update.split(",")
+            # print(update_list)
+            if first in update_list and last in update_list:
+                # print("they both be in here mon", update_list)
+                pos_first = update_list.index(first)
+                pos_last = update_list.index(last)
+                # if pos_first < pos_last:
+                #     print("ok")
+                    # print("they both be in here mon", update_list)
+                if pos_first > pos_last:
+                    # print("not ok")
+                    # print("not ok", update_list)
+                    updates.remove(update)
+    # print(updates)
+    return updates
+                    
+def find_middle_value(list):
+    sum = 0
+    for line in list:
+        line_list = line.split(",")
+        middle = int(len(line_list) / 2)
+        print("middle item in list", line_list[middle])
+        sum += int(line_list[middle])
+    print(sum)
+    return sum
 
 if __name__ == "__main__":
     rules, updates = open_file("exampledata.txt")
-    print_contents(rules)
-    print_contents(updates)
-    check_order(rules, updates)
+    # print_contents(rules)
+    # print_contents(updates)
+    ok_list = check_order(rules, updates)
+    print_contents(ok_list)
+    find_middle_value(ok_list)
 
 
