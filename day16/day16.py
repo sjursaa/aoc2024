ac44f30524728e3ac822495b38a4c4f3870d9cdb
Sjur <sjursaa@gmail.com>
2024-12-26 19:43:47 +0100

    day16: get start and end position when importing puzzle input

--- day16/day16.py
+++ day16/day16.py
@@ -3,17 +3,29 @@ file = open("exampledata.txt")
 array2d = []
 
 def import_file():
-    for line in file:
+    start_pos = []
+    end_pos = []
+    for i, line in enumerate(file):
         list = []
-        for char in line.strip():
+        for j, char in enumerate(line.strip()):
             list.append(char)
+            if char == "E":
+                end_pos = [i, j]  
+            if char == "S":
+                start_pos = [i, j]
+
         array2d.append(list)
+    return start_pos, end_pos
 
 def print_whole_array():
     for line in array2d:
         print(line)
 
 if __name__ == "__main__":
-    import_file()
+    vals = import_file()
+    # print(vals)
     print_whole_array()
+    print(vals)
+    # print(start_pos)
+    # print(end_pos)
 
